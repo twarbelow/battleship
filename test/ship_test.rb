949892cde40f17522e2605a261a011571d09a060
Michael Chickillo <truthvendor@gmail.com>
2020-05-12 11:22:35 -0600

    first 2 tests completed

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -5,40 +5,41 @@ require './lib/ship'
 class Shiptest < Minitest::Test
 
   def setup
-    cruiser = Ship.new("Cruiser", 3)
-
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_it_exists
-
+    assert_instance_of Ship, @cruiser
   end
 
-  def test_has_length
-
-
-    assert_equal 3, ship.length
+  def test_it_has_a_name
+    assert_equal "Cruiser", @cruiser.name
   end
-
-  def test_it_has_health
-
-
-    assert_equal 3, ship.health
-  end
-
-  def test_it_is_not_sunk_to_start
-
+  #
+  # def test_has_length
+  #
+  #
+  #   assert_equal 3, @cruiser.length
+  # end
+  #
+  # def test_it_has_health
+  #
+  #
+  #   assert_equal 3, @cruiser.health
+  # end
+  #
+  # def test_it_is_not_sunk_to_start
+  #
+  #
+  #   assert_equal false, @cruiser.sunk?
+  # end
+  #
+  def test_it_gets_hit
+    ship.hit
 
     assert_equal false, ship.sunk?
+    assert_equal 2, ship.health
   end
-
-  # def test_it_gets_hit
-  #
-  #
-  #   ship.hit
-  #   assert_equal false, ship.sunk?
-  #   assert_equal 2, ship.health
-  #
-  # end
 
   def test_ship_gets_sunk
 
