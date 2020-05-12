bb49ef85ab7a24ef263f93f0ef792f7756ca2518
Michael Chickillo <truthvendor@gmail.com>
2020-05-12 08:29:23 -0600

    INitial commit

---
+++ test/cell_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class Celltest < Minitest::Test
+
+  def setup
+    cell = Cell.new("B4")
+
+  end
+
+  def test_it_exists
+
+  end
+
+  def test_it_has_coordinates
+    
+  end
+
+
+end
