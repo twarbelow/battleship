15e8ef668c36138741d1be7924de5d55e9adf7b8
“Taija” <taija.warbelow@google.com>
2020-05-13 16:49:50 -0700

    set up test_number_of_placement_coordinates_equals_ship_length, not passing

--- test/board_test.rb
+++ test/board_test.rb
@@ -5,30 +5,74 @@ require './lib/cell'
 require './lib/board'
 
 class BoardTest < Minitest::Test
+  class BasicTests < BoardTest
+    def setup
+      @board = Board.new
 
-  def setup
-    @board = Board.new
+      @cruiser = Ship.new("Cruiser", 3)
+      @submarine = Ship.new("Submarine", 2)
+    end
 
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
+    def test_it_exists
+      assert_instance_of Board, @board
+    end
+
+    def test_it_has_cells
+      assert_equal 16 , @board.cells.count
+      assert_instance_of Cell, @board.cells[:A1]
+    end
+
+    def test_it_has_valid_coordinates?
+      assert_equal true, @board.valid_coordinates?("A1")
+      assert_equal true, @board.valid_coordinates?("D4")
+      assert_equal false, @board.valid_coordinates?("A5")
+      assert_equal false, @board.valid_coordinates?("E1")
+      assert_equal false, @board.valid_coordinates?("A22")
+    end
   end
 
-  def test_it_exists
-    assert_instance_of Board, @board
+  class ValidPlacementTests < BoardTest
+    def setup
+      @board = Board.new
+
+      @cruiser = Ship.new("Cruiser", 3)
+      @submarine = Ship.new("Submarine", 2)
+    end
+
+    def test_number_of_placement_coordinates_equals_ship_length
+      assert_equal false, @board.valid_placement?(cruiser, ["A1", "A2"])
+      assert_equal false, @board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    end
   end
 
-  def test_it_has_cells
-    assert_equal 16 , @board.cells.count
-    assert_instance_of Cell, @board.cells[:A1]
-  end
 
-  def test_it_has_valid_coordinates?
-    assert_equal true, @board.valid_coordinates?("A1")
-    assert_equal true, @board.valid_coordinates?("D4")
-    assert_equal false, @board.valid_coordinates?("A5")
-    assert_equal false, @board.valid_coordinates?("E1")
-    assert_equal false, @board.valid_coordinates?("A22")
-  end
+# To proceed past the above tests, work these patterns in these groupings:
+# NEXT: make sure the coordinates are consecutive
+#   pry(main)> board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+# # => false
+#
+# pry(main)> board.valid_placement?(submarine, ["A1", "C1"])
+# # => false
+#
+# pry(main)> board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+# # => false
+#
+# pry(main)> board.valid_placement?(submarine, ["C1", "B1"])
+# # => false
 
-  
+# THEN: coordinates can’t be diagonal
+# pry(main)> board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+# # => false
+#
+# pry(main)> board.valid_placement?(submarine, ["C2", "D3"])
+# # => false
+
+# FINALLY: these 2 should pass
+# pry(main)> board.valid_placement?(submarine, ["A1", "A2"])
+# # => true
+#
+# pry(main)> board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+# # => true
+
+
 end
