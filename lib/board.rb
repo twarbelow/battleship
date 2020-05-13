d2df975cc372f10f7bf5e2354a569423f001216f
Michael Chickillo <truthvendor@gmail.com>
2020-05-13 12:14:52 -0600

    Commit again for iteration2 creation

---
+++ lib/board.rb
@@ -0,0 +1,46 @@
+require './lib/ship'
+require './lib/cell'
+
+class Board
+  attr_reader
+
+  # def initialize(argument)
+  #   @argument = argument
+  # end
+
+  def valid_coordinate
+
+  end
+
+  def valid_placement?
+
+  end
+
+  # will need to use class Range for testing coordinates
+  # range = "A".."D"
+  # range.to_a
+  # ["A", "B", "C", "D"]
+
+
+#
+
+
+cells = {
+        A1: ".",
+        A2: ".",
+        A3: ".",
+        A4: ".",
+        B1: ".",
+        B2: ".",
+        B3: ".",
+        B4: ".",
+        C1: ".",
+        C2: ".",
+        C3: ".",
+        C4: ".",
+        D1: ".",
+        D2: ".",
+        D3: ".",
+        D4: "."
+}
+end
