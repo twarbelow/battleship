adaf5e1ba7cf2d07a75ab60dda78ba0bf94642c9
“Taija” <taija.warbelow@google.com>
2020-05-14 08:58:47 -0700

    refactor so we are not accessing an instance variable in the hit method. Utilized the private method to create an attr_writer that is not accessible outside the class

--- lib/ship.rb
+++ lib/ship.rb
@@ -14,6 +14,13 @@ class Ship
   end
 
   def hit
-    @health -= 1
+    self.health -= 1
   end
+
+  private
+  attr_writer :health
 end
+
+# @ is available for anything in the class
+# self.health avoids method creating its own local variable (remember scope)
+# everything that comes below `private` will not be accessible outside of the class
