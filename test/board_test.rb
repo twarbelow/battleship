203a7d19cc3b85e5a68d252bd93d46061f67e6e8
Michael Chickillo <truthvendor@gmail.com>
2020-05-15 07:03:42 -0600

    Commit prior to overwrite

--- test/board_test.rb
+++ test/board_test.rb
@@ -44,6 +44,8 @@ class BoardTest < Minitest::Test
       assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
     end
 
+
+
     # def test_cells_must_be_consecutive
     #   assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
     #   assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
