77a11486b45d73ee0e14e86c516035a9fed97f7e
Michael Chickillo <truthvendor@gmail.com>
2020-05-18 15:07:17 -0600

    Initial commit

--- test/game_test.rb
+++ test/game_test.rb
@@ -3,7 +3,16 @@ require 'minitest/pride'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
-require "./lib/game"
+require './lib/game'
 
 class GameTest < Minitest::Test
+
+  def setup
+    @game = Game.new
+  end
+
+  def test_it_has_main_menu
+    expected = "Welcome to BATTLESHIP\n" "Enter p to play. Enter q to quit."
+    assert_equal expected, @game.main_menu
+  end
 end
