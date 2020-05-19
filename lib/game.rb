ea6b999a01cf5bf746be74ebe5075588413ce246
Michael Chickillo <truthvendor@gmail.com>
2020-05-19 17:15:36 -0600

    Added turn functionality

--- lib/game.rb
+++ lib/game.rb
@@ -25,20 +25,57 @@ class Game
       puts "C . . . ."
       puts "D . . . ."
 
-      human.cruiser_place
-      human.submarine.place
+      human.place_cruiser
+      human.place.submarine
 
-      computer.cruiser_place
-      computer.submarine_place
+      computer.place_cruiser
+      computer.place_submarine
 
       turn(computer, human)
     end
   end
 
   def display_boards(computer, human)
-
+    puts "==============COMPUTER BOARD=============="
+    puts computer.board.render
+    puts "==============PLAYER BOARD=============="
+    puts player.board.render(true)
   end
 
+  def turn(computer, player)
+    16.times do
+      display_boards(computer, player)
+
+      loop do
+        puts "Enter the coordinate for your shot."
+        player_input = gets.chomp
+        if computer.board.valid_coordinate?(player_input) == true
+          computer.board.cells[player_input].fire_upon
+          if computer.board.cells[player_input].render(true) == "M"
+            puts "Your shot on #{player_input} was a miss."
+          elsif computer.board.cells[player_input].render(true) == "H"
+            puts "Your shot on #{player_input} was a hit."
+          elsif computer.board.cells[player_input].render(true) == "X"
+            puts "Your shot sunk my #{computer.board.cells.ship.name}!"
+          end
+          break
+        end
+        puts "Please enter a valid coordinate."
+      end
 
 
+
+      computer_input = player.board.cells.keys.sample
+      if player.board.valid_coordinate?(computer_input) == true
+        player.board.cells[computer_input].fire_upon
+        if player.board.cells[computer_input].render(true) == "M"
+          puts "Computer shot on #{computer_input} was a miss."
+        elsif player.board.cells[computer_input].render(true) == "H"
+          puts "Computer shot on #{computer_input} was a hit."
+        elsif player.board.cells[computer_input].render(true) == "X"
+          puts "Computer shot on #{computer_input} sunk your #{player.board.cells.ship.name}!"
+        end
+      end
+    end
+  end
 end
