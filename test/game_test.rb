b485f5bd5297bdf9c1067b21060764f9fa22c186
“Taija” <taija.warbelow@google.com>
2020-05-19 17:58:05 -0700

    work on test_it_has_main_menu. in progress

--- test/game_test.rb
+++ test/game_test.rb
@@ -11,6 +11,12 @@ class GameTest < Minitest::Test
     @game = Game.new
   end
 
-  def test
+  def test_it_has_main_menu
+    # # invalid response gets mock/stub
+    # assert_equal "Invalid response. Please type either p or q.", @game.main_menu
+    # # q input gets mock/stub
+    # assert_equal "Goodbye", @game.main_menu
+    @game.stubs(:gets).returns("P")
+    assert_equal "Ok! Time to set up our boards!", @game.main_menu
   end
 end
