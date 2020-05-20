1425f2d4228c30078cd96399c8ef996198c7e471
“Taija” <taija.warbelow@google.com>
2020-05-20 14:27:08 -0700

    refactor place methods, tests passing

--- lib/human.rb
+++ lib/human.rb
@@ -11,25 +11,25 @@ class Human
 
   def place_cruiser
     loop do
-      puts "Enter the squares for the cruiser (3 spaces). Please do it in this format: B1, B2, B3"
+      p "Enter the squares for the cruiser (3 spaces). Please do it in this format: B1, B2, B3"
       cruiser_coords = gets.chomp.upcase.gsub(',', '').split
       if board.valid_placement?(cruiser, cruiser_coords) == true
         @board.place(cruiser, cruiser_coords)
-        break
+        break("placed")
       end
-      puts "Those are invalid coordinates. Please try again:"
+      p "Those are invalid coordinates. Please try again:"
     end
   end
 
   def place_submarine
     loop do
-      puts "Enter the squares for the submarine (2 spaces). Please do it in this format: C3, D3"
+      p "Enter the squares for the submarine (2 spaces). Please do it in this format: C3, D3"
       submarine_coords = gets.chomp.upcase.gsub(',', '').split
       if board.valid_placement?(submarine, submarine_coords) == true
         @board.place(submarine, submarine_coords)
-        break
+        break("placed")
       end
-      puts "Those are invalid coordinates. Please try again:"
+      p "Those are invalid coordinates. Please try again:"
     end
   end
 end
