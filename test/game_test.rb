64988a020207db34181641cbedae7f9a5d0a1812
“Taija” <taija.warbelow@google.com>
2020-05-19 18:02:05 -0700

    add necessary human and game file requirements

--- test/game_test.rb
+++ test/game_test.rb
@@ -4,11 +4,15 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require './lib/game'
+require './lib/human'
+require './lib/computer'
 
 class GameTest < Minitest::Test
 
   def setup
-    @game = Game.new
+    @human = Human.new
+    @computer = Computer.new
+    @game = Game.new(@human, @computer)
   end
 
   def test_it_has_main_menu
@@ -16,7 +20,7 @@ class GameTest < Minitest::Test
     # assert_equal "Invalid response. Please type either p or q.", @game.main_menu
     # # q input gets mock/stub
     # assert_equal "Goodbye", @game.main_menu
-    @game.stubs(:gets).returns("P")
+    @game.stub(:gets).returns("P")
     assert_equal "Ok! Time to set up our boards!", @game.main_menu
   end
 end
