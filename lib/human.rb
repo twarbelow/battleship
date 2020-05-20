7beb5f7200e2aab2e619427605dba09e263dc8e5
“Taija” <taija.warbelow@google.com>
2020-05-20 13:18:51 -0700

    remove input value from submarine_cruiser, update submarine_coords

--- lib/human.rb
+++ lib/human.rb
@@ -14,7 +14,6 @@ class Human
       puts "Enter the squares for the cruiser (3 spaces). Please do it in this format: B1, B2, B3."
       cruiser_coords = gets.chomp.upcase.gsub(',', '').split
       if board.valid_placement?(cruiser, cruiser_coords) == true
-        require 'pry'; binding.pry
         @board.place(cruiser, cruiser_coords)
         break
       end
@@ -25,8 +24,7 @@ class Human
   def place_submarine
     loop do
       puts "Enter the squares for the submarine (2 spaces):"
-      input = gets.chomp
-      submarine_coords = input.split(" ")
+      submarine_coords = gets.chomp.upcase.gsub(',', '').split
       if board.valid_placement?(submarine, submarine_coords) == true
         @board.place(submarine, submarine_coords)
         break
